cd7303bd8c76d0478174d9233d52d701b9704f66
MitraKumar <kaushikmitra556@gmail.com>
2019-11-10 01:30:51 +0530

    fixed the todos

--- components/Todo.js
+++ components/Todo.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
-import {Button, Typography} from '@material-ui/core/'
+import {Button, Typography, Grid} from '@material-ui/core/'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(1)
   },
 }));
 
@@ -16,12 +17,19 @@ export default function Todo({ todo, onClick, onDelete }) {
   const classes = useStyles()
   return (
     <Paper className={classes.root}>
-      <Typography
-        component="span"
-        style={{ textDecoration: completed ? 'line-through' : '', cursor: 'pointer' }}
-        onClick={() => onClick(id)}
-      >{name}</Typography>
-      <Button onClick={() => onDelete(id)} variant="outlined" color="secondary">X</Button>
+      <Grid container>
+        <Grid item xs={10}>
+          <Typography
+            component="span"
+            style={{ textDecoration: completed ? 'line-through' : '', cursor: 'pointer' }}
+            onClick={() => onClick(id)}
+          >{name}</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Button onClick={() => onDelete(id)} variant="outlined" color="secondary">X</Button>
+        </Grid>
+      </Grid>
+      
     </Paper>
   )
 }
