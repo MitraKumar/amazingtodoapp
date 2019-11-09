50491b7ee594de5fe49a0027480bcdd2af115f9e
MitraKumar <kaushikmitra556@gmail.com>
2019-11-10 01:53:01 +0530

    Fixed the form

--- components/Form.js
+++ components/Form.js
@@ -2,31 +2,54 @@ import React, { useState } from 'react';
 
 import { useTodoValue } from '../contexts/TodoContext'
 
-import {FormControl, TextField, Button} from '@material-ui/core';
+import {FormControl, TextField, Button, Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(themes => ({
+  inputField: {
+    width: '100%'
+  } 
+}))
 
 export default function Form({onSubmit}) {
   const [name, setName] = useState("")
   const [ _ , dispatch] = useTodoValue()
 
+  const classes = useStyles()
   return (
-    <FormControl>
-      <TextField
-        id="standard-basic"
-        label="Enter a todo..."
-        margin="normal"
-        onChange={e => setName(e.target.value)}
-        value={name}
-      />
-      
-      <Button
-        variant="contained" 
-        color="primary"
-        onClick={e => {
-          e.preventDefault()
-          dispatch({ type: "add", payload: name })
-          setName("")
-        }}
-      >Save</Button>
-    </FormControl>
+    <Grid container>
+      <Grid item xs={12}>
+        <form onSubmit={e => {
+            e.preventDefault()
+            dispatch({ type: "add", payload: name })
+            setName("")
+        }}>
+          <TextField
+            className={classes.inputField}
+            id="standard-basic"
+            label="Enter a todo..."
+            margin="normal"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+        </form>
+      </Grid>
+    </Grid>
   )
 }
+
+
+/**
+ * 
+ <Grid item xs={4}>
+        <Button
+          variant="contained" 
+          color="primary"
+          onClick={e => {
+            e.preventDefault()
+            dispatch({ type: "add", payload: name })
+            setName("")
+          }}
+        >Save</Button>
+      </Grid>
+ */
